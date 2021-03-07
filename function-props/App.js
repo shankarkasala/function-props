61c14406cdb83bb9127ef9154036016ece802320
shankarkasala <80156261+shankarkasala@users.noreply.github.com>
2021-03-07 13:49:10 +0530

    initial commit

---
+++ function-props/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Propsfn from './components/Propsfn';
+
+function App() {
+
+  return (
+    <div className="App">
+   <Propsfn name='React'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ function-props/Propsfn.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const heading={
+    fontSize:'30px',
+    color:'green'
+}
+const Propsfn =(props)=><h1 style={heading}>Hello {props.name} </h1>
+
+export default Propsfn
